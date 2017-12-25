745e0ebd57973db4a20192ec896eb655907b969b
Anton <newaitix@gmail.com>
2017-12-25 13:05:06 +0200

    1

--- library.js
+++ library.js
@@ -16,7 +16,6 @@
 			status:'alpha',
 			version:'3.2.3.171223'
 		},
-		
 		inc=typeof module==='object'&&typeof module.exports==='object'?module.exports=global.document?factory(global,factory):function(w){if(!w.document)j.error(j.message.ad);return factory(w,factory);}():factory(global,factory);
 	typeof global.jsl==='undefined'?(inc.fn=j,global.jsl=inc,document.addEventListener('DOMContentLoaded',function(){inc.fn.DOMCL=true;},false)):j.error(j.message.ac);
 }(typeof window!='undefined'?window:this,function(ww,fn){
